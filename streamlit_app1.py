d81bcac397410dc1fadc719fa9a8cd79a890864e
syl3sh <sylesh2206@gmail.com>
2026-06-26 10:19:49 +0800

    Update base URL for web API connection

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -8,7 +8,7 @@ import time
 import subprocess
 st.header("NAS System Dashboard", divider="rainbow")
 
-base = "http://192.168.11.228:5000/webapi"
+base = "http://QuickConnect.to/Testsvrs:5000/webapi"
 wifi = st.secrets["secrets"]["wifiname"]
 wifipassword=st.secrets["secrets"]["wifipasswd"]
 
