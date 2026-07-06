24ecd9d2ac21cbdf224a3a3f790ea7602cce235d
syl3sh <sylesh2206@gmail.com>
2026-07-06 15:21:54 +0800

    Update streamlit_app1.py

--- pages/streamlit_app1.py
+++ pages/streamlit_app1.py
@@ -18,7 +18,9 @@ sgt = pytz.timezone("Asia/Singapore")
 
 st.header("NAS Syst Dashboard", divider="rainbow")
 base = "http://testsvrs.synology.me:5000/webapi"
-
+NAS_HOST = "testsvrs.synology.me"
+NAS_PORT = "5000"
+RESTART_TASK_NAME = "Dashboard Scheduled Restart"
 
 config = {
     "credentials": {
