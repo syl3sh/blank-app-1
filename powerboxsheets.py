502d5885e0765b925ea467ae8e0164f35661d267
syl3sh <sylesh2206@gmail.com>
2026-07-21 15:54:31 +0800

    Change Excel file path and display DataFrame
    Updated the path for the Excel file and modified the way the DataFrame is displayed.

--- powerboxsheets.py
+++ powerboxsheets.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-df = pd.read_excel('ATE_Tracking_Record_10726.xlsx')
-st.write(df.head())
-time.sleep(30)
-st.rerun()
+df = pd.read_excel('data/ATE_Tracking_Record_10726.xlsx')
+st.write(df)
+
 
