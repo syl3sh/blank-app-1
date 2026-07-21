779d914e272bad36b08bfae5b659ce7b90fd4752
syl3sh <sylesh2206@gmail.com>
2026-07-21 16:37:15 +0800

    Update powerboxsheets.py

--- powerboxsheets.py
+++ powerboxsheets.py
@@ -2,9 +2,10 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 df = pd.read_excel('data/ATE_Tracking_Record_10726.xlsx')
-st.dataframe(df.fillna(""))
+
 edited_df = st.data_editor(df, num_rows = "dynamic")
 
 if st.button("Save Changes"):
   edited_df.to_excel("data/ATE_Tracking_Record_10726.xlsx", index = False)
+  st.dataframe(edited_df.fillna(""))
   st.success("Saved")
