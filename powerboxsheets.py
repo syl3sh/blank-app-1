83cd01404aef65719a74fa9ca7f2e43116f8e0ea
syl3sh <sylesh2206@gmail.com>
2026-07-21 14:59:07 +0800

    Update DataFrame display and add rerun functionality
    Replaced print with write for displaying DataFrame and added sleep and rerun functionality.

--- powerboxsheets.py
+++ powerboxsheets.py
@@ -1,6 +1,8 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 df = pd.read_excel('ATE_Tracking_Record_10726.xlsx')
-st.print(df.head())
-
+st.write(df.head())
+time.sleep(30)
+st.rerun()
 
