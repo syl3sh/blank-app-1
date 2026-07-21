4232b594bc70b1495e8b8d906263c3a071b48045
syl3sh <sylesh2206@gmail.com>
2026-07-21 16:33:29 +0800

    Update powerboxsheets.py

--- powerboxsheets.py
+++ powerboxsheets.py
@@ -4,9 +4,7 @@ import pandas as pd
 df = pd.read_excel('data/ATE_Tracking_Record_10726.xlsx')
 st.dataframe(df.fillna(""))
 edited_df = st.data_editor(df, num_rows = "dynamic")
-if st.button("Save changes"):
-    edited_df.to_excel("data/mydata.xlsx", index=False)
-    st.success("Saved!")
+
 if st.buttons("Save Changes"):
   edited_df.to_excel("data/ATE_Tracking_Record_10726.xlsx", index = False)
   st.success("Saved")
