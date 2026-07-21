da061d22de9fcd7eb5b5e4a448fdc73e37e62ff0
syl3sh <sylesh2206@gmail.com>
2026-07-21 16:07:50 +0800

    Update powerboxsheets.py

--- powerboxsheets.py
+++ powerboxsheets.py
@@ -4,4 +4,6 @@ import pandas as pd
 df = pd.read_excel('data/ATE_Tracking_Record_10726.xlsx')
 st.write(df)
 df = df.fillna("") 
-
+df = df.dropna(axis=0, how="all")
+df = df.dropna(axis=1, how="all")
+df = df.dropna(axis=3, how="all")
